1c1825da92dd8762642d6cbe0dc33a4b7b6eddb4
emptyloot <daviddcmmoo@gmail.com>
2025-12-04 10:41:49 -0800

    Add components to scroll on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
-//import Hero from "@/components/sections/Hero";
+import Hero from "@/components/sections/Hero";
+import About from "@/components/sections/About";
 import Projects from "@/components/sections/Projects";
-//import Diagnostics from "@/components/sections/Diagnostics";
+import Diagnostics from "@/components/sections/Diagnostics";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full">
+      
+      <Hero />
+      <About />
       <Projects />
+      <Diagnostics /> 
+      
+
     </div>
   );
 }
